c9d98f9507a264d9ebb10c8c0be917457f82e29f
ALTAF <altafhossen.pro@gmail.com>
2026-07-08 13:31:33 +0600

    updated header and img issue

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-        domains: ['localhost', '127.0.0.1','res.cloudinary.com','64.227.133.212','forpink.com','www.forpink.com','api.forpink.com'],
+        domains: ['localhost', '127.0.0.1','res.cloudinary.com','64.227.133.212','pinkspot.bd','www.pinkspot.bd','api.pinkspot.bd', 'forpink.com','www.forpink.com','api.forpink.com'],
     },
 };
 
